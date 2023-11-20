ced0a0c5ca738f648b3c4e321c3c8474f94361d4
lenoci-alice <alicelenoci.99@gmail.com>
2023-11-20 15:48:45 +0100

    FUNZIONE KNOB + libreria (da controllare), html e css

--- loopStation/js/main.js
+++ loopStation/js/main.js
@@ -9,3 +9,19 @@ import { View, ChannelHandler, TopBarHandler } from "./View/View.js";
 
  // init vari
 }
+
+var knob = document.getElementById("knob");
+Draggable.create(knob, {
+  type:"rotation",
+  bounds:{minRotation:0, maxRotation:270},
+  onDrag:function() {
+    var rotation = parseInt(this.rotation % 360, 10);
+    /*output.innerHTML = (rotation < 0) ? rotation + 360 : rotation;*/
+  }
+});
+
+// NON CREDO SI FACCIA COSI QUINDI LO LASCIO COME COMMENTO
+//import 'https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.1/TweenMax.min.js'
+//import 'https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.1/utils/Draggable.min.js'>
+//import 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/ThrowPropsPlugin.min.js'>
+
