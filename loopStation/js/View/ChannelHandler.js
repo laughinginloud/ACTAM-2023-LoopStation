f22f33c89614022964a48d3d047b78fb823daf62
Mattia Martelli <mattia.martelli@live.it>
2023-12-07 17:47:17 +0100

    Corretta illuminazione pulsante play/pause

--- loopStation/js/View/ChannelHandler.js
+++ loopStation/js/View/ChannelHandler.js
@@ -3,42 +3,56 @@ class ChannelHandler {
   flags;
   recordPlayButton;
 
+  channelIndex;
+
   constructor(channel, index) {
-    this.channel = channel;
-    this.flags   = channel.player.flags;
+    this.channel      = channel;
+    this.flags        = channel.player.flags;
+    this.channelIndex = index;
 
     document.getElementById("rec" + index).addEventListener("click", this.recordPlayButtonHandler);
-    document.getElementById("vol" + index).children[0].addEventListener("input", () => this.gainHandler(index));
+    document.getElementById("vol" + index).children[0].addEventListener("input", this.gainHandler);
     document.getElementById("sp" + index).addEventListener("click", this.playPauseButtonHandler);
     document.getElementById("clear" + index).addEventListener("click", this.clearHandler);
   }
 
   recordPlayButtonHandler = () => {
-    if (this.flags.rec)
+    if (this.flags.rec) {
       this.channel.player.stopRecord();
+      document.getElementById("sp" + this.channelIndex).classList.add("modifica");
+    }
 
-    else
+    else {
       this.channel.player.startRecord(this.channel.player.play);
+      document.getElementById("sp" + this.channelIndex).classList.remove("modifica");
+    }
   }
 
-  gainHandler = index => {
+  gainHandler = () => {
     // @ts-ignore
-    this.channel.changeGain(Number(document.getElementById("vol" + index).children[0].value) / 100)
+    this.channel.changeGain(Number(document.getElementById("vol" + this.channelIndex).children[0].value) / 100)
   }
 
   playPauseButtonHandler = () => {
-    if (this.flags.rec)
+    if (this.flags.rec) {
       this.channel.player.stopRecord();
+      document.getElementById("sp" + this.channelIndex).classList.add("modifica");
+    }
 
-    else if (this.flags.play)
+    else if (this.flags.play) {
       this.channel.player.pause();
+      document.getElementById("sp" + this.channelIndex).classList.remove("modifica");
+    }
 
-    else if (!this.flags.play)
+    else if (!this.flags.play) {
       this.channel.player.play();
+      document.getElementById("sp" + this.channelIndex).classList.add("modifica");
+    }
   }
 
   clearHandler = () => {
     this.channel.player.clean();
+    document.getElementById("sp" + this.channelIndex).classList.remove("modifica");
   }
 }
 
